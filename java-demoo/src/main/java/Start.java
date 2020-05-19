e8fa0f25ed319a87b595c49fb6cb76646473355e
“ <cbeann@163.com>
2020-05-20 00:20:49 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -7,9 +7,9 @@ public class Start {
     public static void main(String[] args) {
 
         System.out.println(111111);
+        System.out.println(123);
         System.out.println(111111);
-        System.out.println(111111);
-        System.out.println(111111);
+        System.out.println(321);
         System.out.println(1);
 
     }
