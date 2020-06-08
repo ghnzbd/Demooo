4227c6a4196096dd8ee3a8db4ef8b538a7f31522
“ <cbeann@163.com>
2020-06-08 23:41:24 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -1,34 +1,11 @@
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
 /**
  * @author CBeann
  * @create 2020-02-20 2:49
  */
 public class Start {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
 
-        LocalTime now = LocalTime.now();
-        System.out.println(now);
-
-        LocalDate localDate = LocalDate.now();
-        LocalDate localDate2 = LocalDate.of(2020, 6, 6);
-        System.out.println(localDate);
-        LocalDateTime localDateTime =LocalDateTime.now();
-        System.out.println(localDateTime);
-        int i = localDate.compareTo(localDate2);
-        System.out.println(i);
-
-        // 根据系统时间返回当前时间并设置为UTC。
-        Clock clock = Clock.systemUTC();
-        System.out.println("Clock : " + clock);
-
-        // 根据系统时钟区域返回时间
-        Clock defaultClock = Clock.systemDefaultZone();
-        System.out.println("Clock : " + clock);
 
 
     }
