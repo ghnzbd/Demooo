a3a11608b8c8ad3156d4559b73f9467be6a5ef7c
cbeann <cbeann@163.com>
2020-06-09 00:50:51 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -7,8 +7,6 @@ public class Start {
     public static void main(String[] args) throws Exception{
         System.out.println("你你你");
         System.out.println("11");
-
-
         System.out.println("87654321");
 
     }
