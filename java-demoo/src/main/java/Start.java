b633f4196c706aa8a9b615a2ce1857694ffa4ae2
“ <cbeann@163.com>
2020-06-09 00:36:28 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -6,12 +6,6 @@ public class Start {
 
     public static void main(String[] args) throws Exception{
         System.out.println("你你你");
-        System.out.println("去去去去我我我");
-        System.out.println("1");
-        System.out.println("123");
-        Start start = new Start();
-        System.out.println("1");
-        System.out.println("2");
 
     }
 }
