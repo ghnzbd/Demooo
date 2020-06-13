60e1879f269d517cfb51648e6f5105b3ef605b11
cbeann <cbeann@163.com>
2020-06-13 18:00:10 +0800

    1

--- springboot-demo-jwt/src/main/java/com/example/controller/ControllerException.java
+++ springboot-demo-jwt/src/main/java/com/example/controller/ControllerException.java
@@ -6,7 +6,6 @@ package com.example.controller;
  */
 
 import com.example.entity.JsonResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.MediaType;
@@ -15,14 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
 public class ControllerException implements ErrorController {
+
+
     private final String PATH = "/error";
 
-    @Autowired
+    private final String MESSAGE = "message";
+
+
+    @Resource
     private ErrorAttributes errorAttributes;
 
     @Override
@@ -31,18 +36,23 @@ public class ControllerException implements ErrorController {
     }
 
     @RequestMapping(value = PATH, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Object handlerError(HttpServletRequest request){
+    public Object handlerError(HttpServletRequest request) {
         Map<String, Object> attributesMap = getErrorAttributes(request, true);
-        System.out.println(attributesMap.get("message"));
-        String msg = null;
-        if (attributesMap.get("message").toString().indexOf("JWT expired") != -1) msg = "JWT已过期";
-        else if (attributesMap.get("message").toString().indexOf("JWT strings must contain exactly 2 period characters") != -1) msg = "JWT格式错误";
-        else if(attributesMap.get("message").toString().indexOf("No message available") != -1) msg = "参数值缺失";
-        else msg = attributesMap.get("message").toString();
+        System.out.println(attributesMap.get(MESSAGE));
+        String msg ;
+        if (attributesMap.get(MESSAGE).toString().contains("JWT expired")) {
+            msg = "JWT已过期";
+        } else if (attributesMap.get(MESSAGE).toString().contains("JWT strings must contain exactly 2 period characters")) {
+            msg = "JWT格式错误";
+        } else if (attributesMap.get(MESSAGE).toString().contains("No message available")) {
+            msg = "参数值缺失";
+        } else {
+            msg = attributesMap.get(MESSAGE).toString();
+        }
         return JsonResult.toJSONObject(Integer.parseInt(attributesMap.get("status").toString()), msg, attributesMap.get("message"));
     }
 
-    protected Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace){
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
         WebRequest webRequest = new ServletWebRequest(request);
         return this.errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
     }
