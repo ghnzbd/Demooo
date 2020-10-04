c2abc356b77459ab47e23b18fee2eaac13368897
cbeann <cbeann@163.com>
2020-10-04 19:37:33 +0800

    eureka-demo

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -1,14 +1,34 @@
-import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.*;
 
 /**
  * @author CBeann
  * @create 2020-02-20 2:49
  */
-@Slf4j
 public class Start {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
+
+    ExecutorService executorService =
+        new ThreadPoolExecutor(
+            2,
+            5,
+            2L,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>(3),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    for (int i = 0; i < 5; i++) {
+      Runnable runnable =
+          new Runnable() {
+            @Override
+            public void run() {
 
 
+            }
+          };
+
+      executorService.submit(runnable);
+    }
   }
 }
--- java-demoo/src/main/java/leetcode/Solution.java
+++ java-demoo/src/main/java/leetcode/Solution.java
@@ -1,40 +1,109 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
   public static void main(String[] args) {
-
     Solution solution = new Solution();
-    System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
   }
 
-  public int lengthOfLongestSubstring(String s) {
+  public int reversePairs2(int[] nums) {
 
-    int length = s.length();
-    int start = -1;
+    return -1;
+  }
 
-    int result = 0;
+  public int reversePairs(int[] nums) {
 
-    Map<Character, Integer> cache = new HashMap<>();
-
-    for (int i = 0; i < length; i++) {
-
-      char c = s.charAt(i);
-      if (cache.containsKey(c)) {
-
-        Integer integer = cache.get(c);
-
-        if (integer >= start) {
-          start = integer;
-        }
-      }
-      cache.put(c, i);
-
-      result = result > (i - start) ? result : (i - start);
+    int len = nums.length;
+    if (len <= 1) {
+      return 0;
     }
 
-    return result;
+    int[] copy = new int[len];
+    for (int i = 0; i < len; i++) {
+      copy[i] = nums[i];
+    }
+
+    int[] temp = new int[len];
+    return reversePairs(copy, 0, len - 1, temp);
+  }
+
+  public int reversePairsM(int[] nums) {
+    int len = nums.length;
+
+    if (len < 2) {
+      return 0;
+    }
+
+    int[] copy = new int[len];
+    for (int i = 0; i < len; i++) {
+      copy[i] = nums[i];
+    }
+
+    int[] temp = new int[len];
+    return reversePairs(copy, 0, len - 1, temp);
+  }
+
+  /**
+   * nums[left..right] 计算逆序对个数并且排序
+   *
+   * @param nums
+   * @param left
+   * @param right
+   * @param temp
+   * @return
+   */
+  private int reversePairs(int[] nums, int left, int right, int[] temp) {
+    if (left == right) {
+      return 0;
+    }
+
+    int mid = left + (right - left) / 2;
+    int leftPairs = reversePairs(nums, left, mid, temp);
+    int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+    if (nums[mid] <= nums[mid + 1]) {
+      return leftPairs + rightPairs;
+    }
+
+    int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+    return leftPairs + rightPairs + crossPairs;
+  }
+
+  /**
+   * nums[left..mid] 有序，nums[mid + 1..right] 有序
+   *
+   * @param nums
+   * @param left
+   * @param mid
+   * @param right
+   * @param temp
+   * @return
+   */
+  private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+    for (int i = left; i <= right; i++) {
+      temp[i] = nums[i];
+    }
+
+    int i = left;
+    int j = mid + 1;
+
+    int count = 0;
+    for (int k = left; k <= right; k++) {
+
+      if (i == mid + 1) {
+        nums[k] = temp[j];
+        j++;
+      } else if (j == right + 1) {
+        nums[k] = temp[i];
+        i++;
+      } else if (temp[i] <= temp[j]) {
+        nums[k] = temp[i];
+        i++;
+      } else {
+        nums[k] = temp[j];
+        j++;
+        count += (mid - i + 1);
+      }
+    }
+    return count;
   }
 }
