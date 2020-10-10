9134e9d059bbe017c46e6eebe88f6086122a6d6a
cbeann <cbeann@163.com>
2020-10-10 21:07:57 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -1,34 +1,10 @@
-import java.util.concurrent.*;
-
 /**
  * @author CBeann
  * @create 2020-02-20 2:49
  */
 public class Start {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 
-    ExecutorService executorService =
-        new ThreadPoolExecutor(
-            2,
-            5,
-            2L,
-            TimeUnit.SECONDS,
-            new LinkedBlockingQueue<Runnable>(3),
-            Executors.defaultThreadFactory(),
-            new ThreadPoolExecutor.AbortPolicy());
-
-    for (int i = 0; i < 5; i++) {
-      Runnable runnable =
-          new Runnable() {
-            @Override
-            public void run() {
-
-
-            }
-          };
-
-      executorService.submit(runnable);
-    }
   }
 }
