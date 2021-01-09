70c40debe0b039f65334aff1ba8c83bbff47f027
cbeann <cbeann@163.com>
2021-01-09 14:55:06 +0800

    1

--- java-demoo/src/main/java/Start.java
+++ java-demoo/src/main/java/Start.java
@@ -7,37 +7,37 @@ public class Start {
   public static void main(String[] args) {
 
     Start start = new Start();
-    // int[] data = new int[] {2, 3, 1, 1, 4};
-    // int[] data = new int[] {0};
-    int[] data = new int[] {1, 1};
-    System.out.println(start.firstMissingPositive(data));
+
+    int[][] data = new int[][] {{1, 4}, {0, 4}};
   }
 
-  private void swap(int[] nums, int index1, int index2) {
-    int temp = nums[index1];
-    nums[index1] = nums[index2];
-    nums[index2] = temp;
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+
+    int step = -1;
+
+    int[][] res = new int[intervals.length + 1][2];
+
+    for (int[] interval : intervals) {
+
+      if (step==-1||res[step][2]>1){
+        //空或者
+      }
+
+
+    }
+
+    return null;
   }
 
-  public int firstMissingPositive(int[] nums) {
-    int length = nums.length;
 
-    for (int i = 0; i < length; i++) {
-
-      int val = nums[i];
-
-      if (val > 0 && val <= length && nums[val - 1] != val) {
-
-        swap(nums, val - 1, i);
-        i--;
-      }
-    }
-
-    for (int i = 0; i < length; i++) {
-      if (nums[i] != i + 1) {
-        return i + 1;
-      }
-    }
-    return length + 1;
+  public void generateList() {
+    //    int[] data = new int[] {1, 2, 3, 4, 5};
+    //    ListNode[] nodes = new ListNode[data.length];
+    //    for (int i = 0; i < data.length; i++) {
+    //      nodes[i] = new ListNode(data[i]);
+    //    }
+    //    for (int i = 0; i < data.length - 1; i++) {
+    //      nodes[i].next = nodes[i + 1];
+    //    }
   }
 }
