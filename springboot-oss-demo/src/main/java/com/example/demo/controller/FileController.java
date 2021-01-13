708b2cb107024b6c264bc7ebcac664fe83803ebc
cbeann <cbeann@163.com>
2021-01-13 14:38:05 +0800

    1

--- springboot-oss-demo/src/main/java/com/example/demo/controller/FileController.java
+++ springboot-oss-demo/src/main/java/com/example/demo/controller/FileController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.utils.OssUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,9 +17,9 @@ public class FileController {
   @Autowired private OssUtils ossUtils;
 
   @PostMapping("/upload")
-  public String upload(MultipartFile file, String path) throws Exception {
+  public String upload(@RequestParam("file") MultipartFile file, String path) throws Exception {
 
-    if (file.isEmpty()) {
+    if (file == null || file.isEmpty()) {
       return "上传文件为空";
     }
 
