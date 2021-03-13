151c48c79603bfb8474cbc16fc2b3449e110d4a5
chairongdian <chairongdian@kuaishou.com>
2021-03-13 15:10:03 +0800

    WindowLimitDemo

---
+++ springboot-demo/src/main/java/com/example/windowlimit/WindowLimitDemo1Controller.java
@@ -0,0 +1,114 @@
+package com.example.windowlimit;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WindowLimitDemo1Controller {
+
+
+    private static final String USER_PREFIX = "user:";
+
+    private static final Integer LIMIT_NUM = 3;
+
+    //1小时的毫秒数
+    private static final Integer PERIOD = 1 * 60 * 60 * 1000;
+    //1分钟
+    private static final Integer PERIOD_WINDOW = 10 * 1000;
+
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    /**
+     * 场景一：1:59分发3条，2:01分发3条成立
+     */
+    @GetMapping("/emailLimit")
+    public Object emailLimit(String userName) {
+
+
+        userName = "zhangsan";
+
+        DateFormat dateTimeInstance = DateFormat.getDateInstance();
+        Date date = new Date();
+        String format = dateTimeInstance.format(date);
+
+        //拼接字符串
+        String key = USER_PREFIX + format + userName;
+
+        String s = stringRedisTemplate.opsForValue().get(key);
+
+
+        Integer num = 0;
+
+        if (null != s) {
+            num = Integer.parseInt(s);
+        }
+
+
+        if (num < LIMIT_NUM) {
+            System.out.println("发送短信");
+            //设置超时
+            stringRedisTemplate.opsForValue().set(key, String.valueOf(num + 1), 1, TimeUnit.HOURS);
+            return 1;
+        } else {
+            return 0;
+        }
+
+
+    }
+
+
+    /**
+     * 1:59分发3条，2:01分发3条不成立，因为在1:50到2:10这个窗口时间段里发送了6条
+     * 下面按照1分钟3条写demo
+     */
+    @GetMapping("/emailWindowLimit")
+    public Object emailWindowLimit(String userName) {
+
+
+        userName = "lisi";
+
+
+        //拼接字符串
+        String key = USER_PREFIX + userName;
+
+
+        long current = System.currentTimeMillis();
+
+        // 移除时间窗口之前的行为记录，剩下的都是时间窗口内的
+        redisTemplate.opsForZSet().removeRangeByScore(key, 0, current - PERIOD_WINDOW);
+        // 获取窗口内的行为数量
+        Long zCard = redisTemplate.opsForZSet().zCard(key);
+
+        if (zCard < LIMIT_NUM) {
+            System.out.println("send email");
+
+
+            // 记录行为
+            redisTemplate.opsForZSet().add(key, current, current);
+            // 设置zset过期时间，避免冷用户持续占用内存
+            // 过期时间应该等于时间窗口长度，再多宽限1s
+            redisTemplate.expire(key, PERIOD_WINDOW + 1, TimeUnit.MILLISECONDS);
+            return 1;
+        }
+
+
+        return 0;
+
+
+    }
+}
---
+++ springboot-demo/src/main/java/com/example/windowlimit/WindowLimtApp.java
@@ -0,0 +1,13 @@
+package com.example.windowlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WindowLimtApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WindowLimtApp.class, args);
+
+    }
+}
